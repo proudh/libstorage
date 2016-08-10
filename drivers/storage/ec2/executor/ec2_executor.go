d3e8b9a076e5bd67bba728d3f518d141da61ada5
Proud Heng <proud.heng@gmail.com>
2016-08-10 16:17:08 +0000

    NextDevice working

--- drivers/storage/ec2/executor/ec2_executor.go
+++ drivers/storage/ec2/executor/ec2_executor.go
@@ -20,6 +20,7 @@ import (
 // driver is the storage executor for the ec2 storage driver.
 type driver struct {
 	config         gofig.Config
+	nextDeviceInfo *types.NextDeviceInfo
 	subnetResolver SubnetResolver
 }
 
@@ -35,6 +36,12 @@ func newDriver() types.StorageExecutor {
 
 func (d *driver) Init(ctx types.Context, config gofig.Config) error {
 	d.config = config
+	d.nextDeviceInfo = &types.NextDeviceInfo{
+		Prefix:  "xvd",
+		Pattern: "[a-z]",
+		Ignore:  false,
+	}
+
 	return nil
 }
 
@@ -51,10 +58,7 @@ func InstanceID() (*types.InstanceID, error) {
 func (d *driver) InstanceID(
 	ctx types.Context,
 	opts types.Store) (*types.InstanceID, error) {
-	/*	iid := &types.InstanceID{Driver: ec2.Name}
-		iid.MarshalMetadata("subnet-c9387cac")
-		return iid, nil
-	*/subnetID, err := d.subnetResolver.ResolveSubnet()
+	subnetID, err := d.subnetResolver.ResolveSubnet()
 	if err != nil {
 		return nil, goof.WithError("no ec2metadata subnet id", err)
 	}
@@ -68,11 +72,40 @@ func (d *driver) InstanceID(
 }
 
 // NextDevice returns the next available device.
-// TODO not implemented yet
 func (d *driver) NextDevice(
 	ctx types.Context,
 	opts types.Store) (string, error) {
-	return "", types.ErrNotImplemented
+	letters := []string{
+		"a", "b", "c", "d", "e", "f", "g", "h",
+		"i", "j", "k", "l", "m", "n", "o", "p"}
+
+	localDeviceNames := make(map[string]bool)
+
+	localDevices, err := d.LocalDevices(
+		ctx, &types.LocalDevicesOpts{Opts: opts})
+	if err != nil {
+		return "", err
+	}
+	localDeviceMapping := localDevices.DeviceMap
+
+	for localDevice, _ := range localDeviceMapping {
+		re, _ := regexp.Compile(`^/dev/` +
+			d.nextDeviceInfo.Prefix +
+			`(` + d.nextDeviceInfo.Pattern + `)`)
+		res := re.FindStringSubmatch(localDevice)
+		if len(res) > 0 {
+			localDeviceNames[res[1]] = true
+		}
+	}
+
+	for _, letter := range letters {
+		if !localDeviceNames[letter] {
+			nextDeviceName := "/dev/" +
+				d.nextDeviceInfo.Prefix + letter
+			return nextDeviceName, nil
+		}
+	}
+	return "", goof.New("No available device")
 }
 
 func (d *driver) LocalDevices(
